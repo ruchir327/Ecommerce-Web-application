3f0cc5553abb95377b67d475d94f2e9b180382f8
Ruchir Shrikhande <72577490+ruchir327@users.noreply.github.com>
2024-03-03 14:38:31 -0500

    Update index.js

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -1,10 +1,1 @@
-import admin from "firebase-admin";
 
-const serviceAccount = require("../config/firebase-key.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fir-829ac.firebaseio.com",
-});
-
-export { admin };
